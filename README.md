0a7bd66e286b730f19f3752bbd5c3a581b47f7f0
JunyaoHu <hujunyao0329@gmail.com>
2026-03-12 18:43:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website: [CVPR 2026] Garments2Look: A Multi-Reference Dataset for High-Fidelity Outfit-Level Virtual Try-On with Clothing and Accessories
+
+See the official repo for more details: https://github.com/ArtmeScienceLab/Garments2Look
---
+++ src/components/sections/Markdown.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ComparisonSection from '../mds/md-comparison.mdx';
+import ConstructionSection from '../mds/md-construction.mdx';
+import BasicInfoSection from '../mds/md-basic-info.mdx';
+import ResultsSection from '../mds/md-results.mdx';
+</script>
+
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+        <ComparisonSection />
+        <el-divider />
+        <ConstructionSection />
+        <el-divider />
+        <BasicInfoSection />
+        <el-divider />
+        <ResultsSection />
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
